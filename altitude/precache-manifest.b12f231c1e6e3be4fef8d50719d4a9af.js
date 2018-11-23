71f27dbe7d1f04497625efbefcb013d78e097983
lazydancer <jrpucula@gmail.com>
2018-11-23 18:45:59 -0500

    altitude added

---
+++ altitude/precache-manifest.b12f231c1e6e3be4fef8d50719d4a9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79c7b90b03b0df5951f5",
+    "url": "./static/css/main.4fe92f4d.chunk.css"
+  },
+  {
+    "revision": "79c7b90b03b0df5951f5",
+    "url": "./static/js/main.79c7b90b.chunk.js"
+  },
+  {
+    "revision": "bbe98d2ae4d00bb48f90",
+    "url": "./static/css/1.f2e52313.chunk.css"
+  },
+  {
+    "revision": "bbe98d2ae4d00bb48f90",
+    "url": "./static/js/1.bbe98d2a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "bfb1b50ee01f90729a13f394d29b45b5",
+    "url": "./index.html"
+  }
+];
